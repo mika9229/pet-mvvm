5c2fcd74c835badc26ba81aa2cd43b49fc56d543
alexandrpryadko <alexpryadko@loudclear.com.au>
2020-09-04 20:33:06 +0300

    initial

---
+++ app/src/main/java/test/com/petmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package test.com.petmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
