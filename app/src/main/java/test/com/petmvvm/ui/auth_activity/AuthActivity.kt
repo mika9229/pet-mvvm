1b53fcb1c43c2b6982809b6671b1e765c9bc9e08
alexandrpryadko <alexpryadko@loudclear.com.au>
2020-11-01 17:21:41 +0200

    remove unused

--- app/src/main/java/test/com/petmvvm/ui/auth_activity/AuthActivity.kt
+++ app/src/main/java/test/com/petmvvm/ui/auth_activity/AuthActivity.kt
@@ -19,7 +19,6 @@ class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding.apply {
             lifecycleOwner = this@AuthActivity
             vm = getViewModel<AuthActivityViewModel>()
